92bb06208514942e8ce711293c42fde837ee2d80
ebarrado <erika_barrado@hotmail.com>
2024-10-22 11:11:31 -0300

    CRIANDO ELEMENTOS DO FORMULÁRIOS E CRIANDO AÇÃO DE ADICIONAR ATIVIDADES

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Map<String, String>> _atividades = [];
+
+  //MÉTODO ADICIONAR ATIVIDADE
+  void _adicionarAtividade(
+      String nome, String descricao, String data, String imagem) {
+    setState(() {
+      _atividades.add({
+        'nome': nome,
+        'descricao': descricao,
+        'data': data,
+        'imagem': imagem
+      });
+    });
+  }
+
   //MÉTODO PARA CADASTRAR ATIVIDADE
   void modalCadastrar(BuildContext context) {
+    //VARIAVEIS PARA FORMULARIO
+    final TextEditingController nomeAtividade = TextEditingController();
+    final TextEditingController descricaoAtividade = TextEditingController();
+    final TextEditingController dataAtividade = TextEditingController();
+    final TextEditingController urlAtividade = TextEditingController();
+
     showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -54,7 +75,57 @@ class _MyHomePageState extends State<MyHomePage> {
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             child: Container(
               child: Column(
-                children: [Text('Conteudo do Modal')],
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Cadastrar Atividade',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      IconButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          icon: Icon(Icons.close))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  //ELEMENTO DO FORMULÁRIO
+                  //LABEL E INPUT
+                  TextField(
+                    controller: nomeAtividade,
+                    decoration: InputDecoration(labelText: 'Nome da Atividade'),
+                  ),
+                  TextField(
+                    controller: descricaoAtividade,
+                    decoration:
+                        InputDecoration(labelText: 'Descricão da Atividade'),
+                  ),
+                  TextField(
+                    controller: dataAtividade,
+                    decoration: InputDecoration(labelText: 'Data da Atividade'),
+                  ),
+                  TextField(
+                    controller: urlAtividade,
+                    decoration: InputDecoration(labelText: 'Url da Atividade'),
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        _adicionarAtividade(
+                            nomeAtividade.text,
+                            descricaoAtividade.text,
+                            dataAtividade.text,
+                            urlAtividade.text);
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.save),
+                      label: const Text("Cadastrar"))
+                ],
               ),
             ),
           );
@@ -73,18 +144,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         backgroundColor: Color.fromARGB(255, 214, 130, 61),
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Atividades('Atividade 1 - Criando Aplicação no Flutter'),
-          Atividades('Atividade 2'),
-          Atividades('Atividade 3'),
-          Atividades('Atividade 4'),
-          Atividades('Atividade 5'),
-          Atividades('Atividade 6'),
-          Atividades('Atividade 7')
-        ],
-      ),
+      body: ListView.builder(
+          itemCount: _atividades.length,
+          itemBuilder: (context, index) {
+            return Atividades(
+              _atividades[index]['nome']!,
+              _atividades[index]['imagem']!,
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           modalCadastrar(context);
@@ -98,14 +165,15 @@ class _MyHomePageState extends State<MyHomePage> {
 class Atividades extends StatelessWidget {
   //variavel para o nome da atividade
   final String nome;
-  const Atividades(this.nome, {Key? key}) : super(key: key);
+  final String imagem;
+  const Atividades(this.nome, this.imagem, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
       child: Container(
-        color: Colors.black87,
+        color: Color.fromARGB(221, 42, 138, 4),
         child: Stack(
           children: [
             Container(
@@ -119,9 +187,13 @@ class Atividades extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
-                    color: Colors.black26,
+                    color: Color.fromARGB(255, 241, 226, 6),
                     width: 72,
                     height: 100,
+                    child: Image.network(
+                      imagem,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                   Container(
                     width: 200,
